2cff5e09ab7b3f321c4ab2a04a408a17b2ed284e
Sofiviola <sofiaviola524@gmail.com>
2024-12-29 11:33:04 -0300

    Traduccion theme cuarta y ultima

---
+++ wp-content/languages/loco/plugins/wp-job-manager-es_AR.l10n.php
@@ -0,0 +1,3 @@
+<?php
+return ['project-id-version'=>'WP Job Manager 2.4.0','report-msgid-bugs-to'=>'https://wordpress.org/support/plugin/wp-job-manager/','last-translator'=>'','language-team'=>'Español de Argentina','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','pot-creation-date'=>'2024-07-25T08:58:08+00:00','po-revision-date'=>'2024-12-29 14:27+0000','x-generator'=>'Loco https://localise.biz/','x-domain'=>'wp-job-manager
+','language'=>'es_AR','plural-forms'=>'nplurals=2; plural=n != 1;','x-loco-version'=>'2.6.14; wp-6.7.1','messages'=>[]];
---
+++ wp-content/languages/loco/plugins/wp-job-manager-tags-es_AR.l10n.php
@@ -0,0 +1,3 @@
+<?php
+return ['project-id-version'=>'WP Job Manager - Job Tags 1.4.6','report-msgid-bugs-to'=>'https://wpjobmanager.com/support/','last-translator'=>'','language-team'=>'Español de Argentina','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','pot-creation-date'=>'2024-04-24T13:19:07+00:00','po-revision-date'=>'2024-12-29 14:30+0000','x-generator'=>'Loco https://localise.biz/','x-domain'=>'wp-job-manager-tags
+','language'=>'es_AR','plural-forms'=>'nplurals=2; plural=n != 1;','x-loco-version'=>'2.6.14; wp-6.7.1','messages'=>[]];
